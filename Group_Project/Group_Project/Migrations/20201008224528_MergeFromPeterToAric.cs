6beffbc3aa924110d9b21c45e7ba012572467edf
Aric <aric@blossom.app>
2020-10-08 16:50:53 -0600

    UserModelCodeChange

---
+++ Group_Project/Group_Project/Migrations/20201008224528_MergeFromPeterToAric.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Group_Project.Migrations
+{
+    public partial class MergeFromPeterToAric : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AddressLine1",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AddressLine2",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Bio",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Link1",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Link2",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Link3",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePic",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "State",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Zip",
+                table: "User",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AddressLine1",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "AddressLine2",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Bio",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Link1",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Link2",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Link3",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "ProfilePic",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Zip",
+                table: "User");
+        }
+    }
+}
