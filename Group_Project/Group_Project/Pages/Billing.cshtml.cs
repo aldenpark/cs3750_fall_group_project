8e5aa0f4a44489c4637a72cc96c3473b2636be09
LAPTOP-V6917H3A\Gobbo <Peterburnett1@mail.weber.edu>
2020-10-08 15:40:11 -0600

    Added non-functional billing page

---
+++ Group_Project/Group_Project/Pages/Billing.cshtml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Threading.Tasks;
+using Group_Project.Data.Repository.IRepository;
+using Group_Project.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Group_Project.Pages
+{
+    public class BillingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+    }
+}
