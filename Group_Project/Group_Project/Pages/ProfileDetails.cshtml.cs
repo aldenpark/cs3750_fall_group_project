fbce468f1c80e35d4a696006f45936f3afbea015
Aric <aric@blossom.app>
2020-10-12 23:35:24 -0600

    ProfileDetails fixed

--- Group_Project/Group_Project/Pages/ProfileDetails.cshtml.cs
+++ Group_Project/Group_Project/Pages/ProfileDetails.cshtml.cs
@@ -1,34 +1,40 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Group_Project.Data;
 using Group_Project.Models;
-
 
 namespace Group_Project.Pages
 {
     public class ProfileDetailsModel : PageModel
     {
-
         private readonly Group_Project.Data.ApplicationDbContext _context;
 
+        public ProfileDetailsModel(Group_Project.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
-        [BindProperty]
         public User User { get; set; }
 
-        public void OnGet()
+        public async Task<IActionResult> OnGetAsync(int? id)
         {
-        }
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-        private bool UserExists(int id)
-        {
-            return _context.User.Any(e => e.ID == id);
-        }
+            User = await _context.User.FirstOrDefaultAsync(m => m.ID == id);
 
+            if (User == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
     }
 }
