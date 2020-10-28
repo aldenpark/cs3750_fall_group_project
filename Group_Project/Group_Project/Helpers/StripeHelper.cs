fa9970ee596e81ac3a8cdc7f483763a58283023e
Aric <aric.olympus@gmail.com>
2020-10-28 16:36:36 -0600

    Stripe Stuff

--- Group_Project/Group_Project/Helpers/StripeHelper.cs
+++ Group_Project/Group_Project/Helpers/StripeHelper.cs
@@ -8,15 +8,23 @@ namespace Group_Project.Helpers
 {
     public class StripeHelper
     {
+        private TokenService tokenService;
+        private ChargeService chargeService;
+        private Token myCardToken;
+        private Charge myChargeToken;
+
         public StripeHelper()
         {
-            StripeConfiguration.ApiKey = "";
+            //Initialize the API Key
+            StripeConfiguration.ApiKey = "sk_test_51HUauxGGhia5bBAKB5Pjb3fkp5OxNTzEZcHDtu2LPSXODAryxlez2qEv7OcOhGAcPzRRpqoOv7XdhKj44aFhWh8L00cdl9rLaO";
+            //Initialize the tokenService
+            tokenService = new TokenService();
+            chargeService = new ChargeService();
         }
 
-       public void SendStripeTokenRequest()
+       public Token SendStripeTokenRequest()
         {
-            StripeConfiguration.ApiKey = "sk_test_4eC39HqLyjWDarjtT1zdp7dc";
-
+            //Make a card
             var options = new TokenCreateOptions
             {
                 Card = new TokenCardOptions
@@ -27,8 +35,52 @@ namespace Group_Project.Helpers
                     Cvc = "314",
                 },
             };
-            var service = new TokenService();
-            service.Create(options);
+
+            
+            
+            //Make request to stripe using a card, retrieve token
+            myCardToken = tokenService.Create(options);
+
+            //Retrieve the Card using the token
+            Token retrievedCard = GetCardById(myCardToken.Id);
+
+            //Charge the card and retrieve the charge token
+            myChargeToken = ChargeCard(myCardToken.Id);
+
+            //Retrieve the charge record using the charge token
+            Charge retrievedCharge = GetChargeById(myChargeToken.Id);
+
+            //Return token
+            return myCardToken;
+        }
+
+        public Token GetCardById(string Id)
+        {
+            Token result = tokenService.Get(Id);
+
+            return result;
+        }
+
+        public Charge ChargeCard(string cardId)
+        {
+            var options = new ChargeCreateOptions
+            {
+                Amount = 2000,
+                Currency = "usd",
+                Source = cardId,
+                Description = "My First Test Charge (created for API docs)",
+            };
+            Charge result = chargeService.Create(options);
+
+            return result;
+        }
+
+        public Charge GetChargeById(string Id)
+        {
+            var service = new ChargeService();
+            Charge result = service.Get(Id);
+
+            return result;
         }
 
 
--- Group_Project/Group_Project/Pages/Stripe/StripeTest.cshtml.cs
+++ Group_Project/Group_Project/Pages/Stripe/StripeTest.cshtml.cs
@@ -2,15 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Group_Project.Helpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Stripe;
 
 namespace Group_Project.Pages.Stripe
 {
     public class StripeTestModel : PageModel
     {
+        [ViewData]
+        public Token stripeToken { get; set; }
         public void OnGet()
         {
+            StripeHelper stripeHelper = new StripeHelper();
+            stripeToken = stripeHelper.SendStripeTokenRequest();
         }
 
 
