cfaa5bda789d4ad89068764c36ccd8a550d07b64
LAPTOP-V6917H3A\Gobbo <Peterburnett1@mail.weber.edu>
2020-11-02 10:07:22 -0700

    CourseID for Assignment

--- Group_Project/Group_Project/Migrations/20201102165124_CourseID.Designer.cs
+++ Group_Project/Group_Project/Migrations/20201102165124_CourseID.Designer.cs
@@ -4,14 +4,16 @@ using Group_Project.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Group_Project.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201102165124_CourseID")]
+    partial class CourseID
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
