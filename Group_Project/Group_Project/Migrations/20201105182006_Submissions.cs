c898dafe0b3aacd360da0560c6307d7edcf3d7b8
Alden Park <aldenpark@gmail.com>
2020-11-05 11:21:56 -0700

    Added submission migration

---
+++ Group_Project/Group_Project/Migrations/20201105182006_Submissions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Group_Project.Migrations
+{
+    public partial class Submissions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Submission",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(nullable: false),
+                    AssignmentId = table.Column<int>(nullable: false),
+                    fileSubmit = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Submission", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Submission");
+        }
+    }
+}
