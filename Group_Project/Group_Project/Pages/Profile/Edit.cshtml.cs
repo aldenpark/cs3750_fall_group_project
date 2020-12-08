3a1b6acbb45ee9045d6e5c32c538a982a39b4a3a
Alden Park <aldenpark@gmail.com>
2020-12-08 12:08:25 -0700

    updated profile

--- Group_Project/Group_Project/Pages/Profile/Edit.cshtml.cs
+++ Group_Project/Group_Project/Pages/Profile/Edit.cshtml.cs
@@ -56,24 +56,14 @@ namespace Group_Project.Pages.Profile
             string webRootPath = _hostingEnvironment.WebRootPath;
             var files = HttpContext.Request.Form.Files; // get, post, put, etc....
 
-            if (HttpContext.Session.GetInt32(SD.UserSessionId) != null)
-            {
-                if (HttpContext.Session.GetInt32(SD.UserSessionId).HasValue)
-                {
-                    int userId = HttpContext.Session.GetInt32(SD.UserSessionId).Value;
-                    User = _unitOfWork.User.Get(userId);
-                }
-            }
-
-            var objFromDb = _unitOfWork.User.GetFirstorDefault(u => u.ID == User.ID);  // Only existing users can get to this page
-
             if (files.Count > 0)
             {
                 string fileName = Guid.NewGuid().ToString();
                 var uploads = Path.Combine(webRootPath, @"img\profile");
                 var extension = Path.GetExtension(files[0].FileName);
 
-                if(objFromDb.ProfilePic != null)
+                var objFromDb = _unitOfWork.User.GetFirstorDefault(u => u.ID == User.ID);  // Only existing users can get to this page
+                if (objFromDb.ProfilePic != null)
                 {
                     var imagePath = Path.Combine(webRootPath, objFromDb.ProfilePic.TrimStart('\\'));
                     if (System.IO.File.Exists(imagePath))
