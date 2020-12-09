496c48bebd1caeef82454b200fde8c9e1bdca09e
Alden Park <aldenpark@gmail.com>
2020-12-08 21:04:44 -0700

    updated delete method for notifications

--- Group_Project/Group_Project/Controllers/NotificationsController.cs
+++ Group_Project/Group_Project/Controllers/NotificationsController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Group_Project.Data.Repository.IRepository;
+using Group_Project.Models;
+using Group_Project.Utility;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,18 +16,70 @@ namespace Group_Project.Controllers
     [ApiController]
     public class NotificationsController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public NotificationsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         // GET: api/<NotificationsController>
         [HttpGet]
         public IActionResult Get()
         {
-            return Json(new { test = "this will be a string of data" });
+
+            int userId = 0;
+            if (HttpContext.Session.GetInt32(SD.UserSessionId) != null)
+            {
+                if (HttpContext.Session.GetInt32(SD.UserSessionId).HasValue)
+                {
+                    userId = HttpContext.Session.GetInt32(SD.UserSessionId).Value;
+                }
+            }
+
+            var notifications = new List<Notification>();
+
+            var courses = new List<Course>();
+            var assignments = new List<Assignment>();
+
+            if (userId > 0)
+            {
+                var Usr = _unitOfWork.User.Get(userId);
+
+                if (Usr.UserType == 'S')
+                {
+                    var submissions = _unitOfWork.Submission.GetAll(x => x.UserId == Usr.ID);
+                    foreach (var submission in submissions)
+                    {
+                        notifications.AddRange(_unitOfWork.Notification.GetAll(x => x.sourceID == submission.AssignmentId));
+                    }
+                    var registrations = _unitOfWork.Registration.GetAll(x => x.StudentID == Usr.ID);
+                    foreach (var registration in registrations)
+                    {
+                        courses.AddRange(_unitOfWork.Course.GetAll(x => x.ID == registration.CourseID));
+                    }
+                    foreach (var course in courses)
+                    {
+                        assignments.AddRange(_unitOfWork.Assignment.GetAll(x => x.CourseID == course.ID));
+                    }
+                    foreach (var assignment in assignments)
+                    {
+                        notifications.AddRange(_unitOfWork.Notification.GetAll(x => x.sourceID == assignment.ID));
+                    }
+                }
+
+
+            }
+
+            return Json(new { notifications });
         }
 
         // DELETE api/<NotificationsController>/5
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
-            var test = id;
+            _unitOfWork.Notification.Remove(id);
+            _unitOfWork.Save();
             return Json(new { success = true });
         }
     }
